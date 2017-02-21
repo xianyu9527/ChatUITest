2efb681b2372f63fd2d96525d97f671fc9824857
qiaozhennan <qznnzq68@163.com>
2017-02-21 16:35:39 +0800

    这次啊，还是一个测试完全无意义

---
+++ app/src/main/java/com/example/qiaozhennan/chatuitest/TestJava.java
@@ -0,0 +1,12 @@
+package com.example.qiaozhennan.chatuitest;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by qiaozhennan on 2017/2/21.
+ */
+
+public class TestJava extends AppCompatActivity {
+
+
+}
